2b6d885832a8905b06e9e61adaf63dd5e926f3c4
alvinfei <alvinfei@tencent.com>
2025-03-28 15:32:03 +0800

    fix

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -8,34 +8,40 @@ local servers = { "html", "cssls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
-		on_attach = on_attach,
-		on_init = on_init,
-		capabilities = capabilities,
-	}
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
 end
 
 -- typescript
 lspconfig.ts_ls.setup {
-	on_attach = on_attach,
-	on_init = on_init,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
 }
 
 --python
 lspconfig.pyright.setup {
-	on_attach = on_attach,
-	on_init = on_init,
-	capabilities = capabilities,
-	filetypes = { "python" },
-	settings = {
-		python = {
-			analysis = {
-				diagnosticSeverityOverrides = {
-					reportOptionalMemberAccess = "warning",
-					reportArgumentType = false,
-				}
-			},
-		},
-	},
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  filetypes = { "python" },
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        diagnosticSeverityOverrides = {
+          reportOptionalMemberAccess = "warning",
+          reportArgumentType = false,
+          reportOptionalSubscript = "warning",
+        },
+-- 关闭不必要的文件监视
+        autoSearchPaths = false,          -- 禁止自动搜索路径
+        diagnosticMode = "openFilesOnly", -- 仅分析打开的文件
+        watchPaths = {"**/requirements.txt"}, -- 明确指定监视的文件（按需调整）
+      },
+    },
+  },
 }
